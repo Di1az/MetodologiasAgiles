0345ea430473387c48d6d6c9357f6db2a03c4110
Di1az <daniel271202@gmail.com>
2024-10-30 14:07:16 -0700

    Fix: Card del proyecto
    Fix para que la card de los proyectos se visualicen

--- electron-sspea/src/renderer.js
+++ electron-sspea/src/renderer.js
@@ -7,6 +7,11 @@ document.getElementById("newProjectBtn").addEventListener("click", () => {
 
 // Mostrar el nuevo proyecto en la interfaz principal
 ipcRenderer.on("new-project", (event, projectData) => {
+
+  const projectContainer = document.getElementById('projectsContainer');
+  const projectCard = createProjectCard(projectData);
+  projectContainer.appendChild(projectCard);
+
   // CAMBIAR FETCH POR APIGATEWAY EN FUTURAS VERSIONES
   //
   console.log(projectData, "data del proy");
@@ -62,7 +67,6 @@ function createProjectCard(projectData) {
   `;
 
   projectCard.querySelector(".edit-btn").addEventListener("click", () => {
-    // Asegurarte de que los datos actuales del proyecto se envíen correctamente al abrir la ventana de edición
     const updatedProjectData = JSON.parse(
       projectCard.dataset.projectData || JSON.stringify(projectData)
     );
